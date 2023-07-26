4b4a1a330c0222392da6414274d03583245c5b57
Momo Kornher <kornherm@amazon.co.uk>
2023-07-26 12:06:18 +0100

    test

--- packages/aws-cdk/lib/build.ts
+++ packages/aws-cdk/lib/build.ts
@@ -14,6 +14,7 @@ export async function buildAllStackAssets(stacks: cxapi.CloudFormationStackArtif
       await buildStackAssets(stack);
     } catch (err) {
       buildingErrors.push(err);
+
     }
   }
 
